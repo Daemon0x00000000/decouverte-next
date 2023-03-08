5b4fbe730721e232415634db4ad40135973737bb
Daemon7895 <mcdaymonz@gmail.com>
2023-03-08 20:42:53 +0100

    config: next experimental app dir + svgr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  experimental: {
+    appDir: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+        test: /\.svg$/,
+        use: [{loader: '@svgr/webpack',options: {icon: true}}]
+    })
+    return config;
+  }
+
 }
 
 module.exports = nextConfig
