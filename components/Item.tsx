794bfb897235a86bc23add774f9fc62922060846
Daemon7895 <mcdaymonz@gmail.com>
2023-03-13 10:05:51 +0100

    edit: Utilisation du context et callback modifié

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,13 +1,17 @@
 import styles from "styles/Tierlist.module.scss";
 import {Draggable} from "react-beautiful-dnd";
 import {FaTrashAlt} from "react-icons/all";
+import ItemProps from "../types/ItemProps";
+import {useContext} from "react";
+import {TierlistContext} from "./TierList";
 
-export default function Item({ id, index, encodedImage, removeCallback,editable}) {
+export default function Item({ id, index, encodedImage, removeCallback}:ItemProps) {
+    const {editable} = useContext(TierlistContext);
     return (
         <Draggable draggableId={id} index={index} isDragDisabled={!editable}>
             {(provider) => (
                 <div className={styles.items} {...provider.draggableProps} {...provider.dragHandleProps} ref={provider.innerRef} style={{backgroundImage: `url(${encodedImage})`,...provider.draggableProps.style}}>
-                    {editable && <button className={styles.deleteItem} onClick={() => removeCallback(id)}><FaTrashAlt/></button>}
+                    {editable && <button className={styles.deleteItem} onClick={() => removeCallback(index)}><FaTrashAlt/></button>}
                 </div>
             )}
         </Draggable>
