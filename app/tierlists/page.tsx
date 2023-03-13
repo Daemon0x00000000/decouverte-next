c107b2f5173767ac6fea4261a983e8ea7b007923
Daemon7895 <mcdaymonz@gmail.com>
2023-03-13 10:02:54 +0100

    reducer: Utilisation du reducer pour la logique d'état de la tierlist

--- app/tierlists/page.tsx
+++ app/tierlists/page.tsx
@@ -1,118 +1,119 @@
 "use client";
 import styles from "styles/Tierlist.module.scss";
 import TierListCP from "../../components/TierList";
-import {useState} from "react";
+import {useReducer} from "react";
+import TierListReducer from "../../reducers/TierListReducer";
 
 
 export default function TierlistPage() {
-     const [tierList,setTierList] = useState({
-        name: "Ma tierlist",
-        tiers: [
-        {
-            name: "S",
-            color: "#FFD700",
-            items: [
-                {
-                    id: "1",
-                    encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                },
-                {
-                    id: "2",
-                    encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                },
-                {
-                    id: "3",
-                    encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                },
-                {
-                    id: "4",
-                    encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                },
-                {
-                    id: "5",
-                    encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                },
-                {
-                    id: "6",
-                    encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                },
-                {
-                    id: "7",
-                    encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                },
-                {
-                    id: "8",
-                    encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                },
-                {
-                    id: "9",
-                    encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                },
-                {
-                    id: "10",
-                    encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                },
-                {
-                    id: "11",
-                    encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                }
-            ]
-        },
-         {
-             name: "SE",
-             color: "#FFD700",
-             items: [
-                 {
-                     id: "01",
-                     encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                 },
-                 {
-                     id: "02",
-                     encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                 },
-                 {
-                     id: "03",
-                     encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                 },
-                 {
-                     id: "04",
-                     encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                 },
-                 {
-                     id: "05",
-                     encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                 },
-                 {
-                     id: "06",
-                     encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                 },
-                 {
-                     id: "07",
-                     encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                 },
-                 {
-                     id: "08",
-                     encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                 },
-                 {
-                     id: "09",
-                     encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                 },
-                 {
-                     id: "010",
-                     encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                 },
-                 {
-                     id: "011",
-                     encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
-                 }
-             ]
-         }
-        ]
-    });
+     const [tierList,tierlistDispatch] = useReducer(TierListReducer,{
+         name: "Ma tierlist",
+         tiers: [
+             {
+                 name: "S",
+                 color: "#FFD700",
+                 items: [
+                     {
+                         id: "1",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "2",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "3",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "4",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "5",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "6",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "7",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "8",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "9",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "10",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "11",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     }
+                 ]
+             },
+             {
+                 name: "SE",
+                 color: "#FFD700",
+                 items: [
+                     {
+                         id: "01",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "02",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "03",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "04",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "05",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "06",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "07",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "08",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "09",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "010",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     },
+                     {
+                         id: "011",
+                         encodedImage: "https://i.imgur.com/1ZQ3wqg.png"
+                     }
+                 ]
+             }
+         ]
+     });
     return (
         <div className={styles.tierlistContainer}>
-            <TierListCP tierList={tierList} setTierList={setTierList} editable={true} />
+            <TierListCP tierList={tierList} tierlistDispatch={tierlistDispatch} editable={true} />
         </div>
     );
 }
