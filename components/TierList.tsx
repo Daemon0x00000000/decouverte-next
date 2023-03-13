d7bcf71b36ce339d04f199897ca9d27e7201ff43
Daemon7895 <mcdaymonz@gmail.com>
2023-03-13 10:04:38 +0100

    edit: Adaptation pour le reducer + context

--- components/TierList.tsx
+++ components/TierList.tsx
@@ -1,16 +1,15 @@
 import TierListProps from "../types/TierListProps";
-import {useEffect, useState} from "react";
-import ItemProps from "../types/ItemProps";
+import {createContext, useEffect, useState} from "react";
 import Tier from "./Tier";
 import styles from "styles/Tierlist.module.scss";
-import {DragDropContext, Droppable} from "react-beautiful-dnd";
-import TierList from "../interfaces/TierListInterface";
+import {DragDropContext, Droppable, DropResult} from "react-beautiful-dnd";
 
-export default function TierListCP({ tierList, setTierList, editable}: TierListProps) {
+export const TierlistContext = createContext({} as TierListProps);
+export default function TierListCP({ tierList, tierlistDispatch, editable}: TierListProps) {
     const [isReady, setIsReady] = useState(false);
-    const [colors, setColors] = useState<string[]>(["#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#0000FF","#4B0082"]);
+    const [colors] = useState<string[]>(["#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#0000FF","#4B0082"]);
 
-    const handleDragEnd = (result: any) => {
+    const handleDragEnd = (result: DropResult) => {
         const { source, destination } = result;
         if (!destination) return;
         if (source.droppableId !== destination.droppableId) {
@@ -20,176 +19,96 @@ export default function TierListCP({ tierList, setTierList, editable}: TierListP
                 const copiedTierList = [...tierList.tiers];
                 const [removed] = copiedTierList.splice(source.index, 1);
                 copiedTierList.splice(destination.index, 0, removed);
-                setTierList((prev:TierList) => ({
-                    ...prev,
-                    tiers: copiedTierList
-                }));
+                tierlistDispatch({
+                    type: "UPDATE_TIERS",
+                    payload: {
+                        tiers: copiedTierList
+                    }
+                });
             } else if (destination.droppableId === "0") {
                 const tier = tierList.tiers.find((tier: { name: any; }) => tier.name === source.droppableId);
                 if (!tier) return;
                 const copiedTierList = [...tierList.tiers];
                 const [removed] = copiedTierList.splice(source.index, 1);
                 copiedTierList.splice(destination.index, 0, removed);
-                setTierList((prev:TierList) => ({
-                    ...prev,
-                    tiers: copiedTierList
-                }));
+                tierlistDispatch({
+                    type: "UPDATE_TIERS",
+                    payload: {
+                        tiers: copiedTierList
+                    }
+                });
             } else {
-                const sourceTier = tierList.tiers.find((tier: { name: any; }) => tier.name === source.droppableId);
-                const destTier = tierList.tiers.find((tier: { name: any; }) => tier.name === destination.droppableId);
-                if (!sourceTier || !destTier) return;
-                const sourceItems = [...sourceTier.items];
-                const destItems = [...destTier.items];
-                const [removed] = sourceItems.splice(source.index, 1);
-                destItems.splice(destination.index, 0, removed);
-                setTierList((prev:TierList) => ({
-                    ...prev,
-                    tiers: prev.tiers.map((tier: { name: any; items: any; }) => {
-                        if (tier.name === source.droppableId) {
-                            return {
-                                ...tier,
-                                items: sourceItems,
-                            };
-                        }
-                        if (tier.name === destination.droppableId) {
-                            return {
-                                ...tier,
-                                items: destItems,
-                            };
-                        }
-                        return tier;
-                    }),
-                }));
+                const copiedTierList = [...tierList.tiers];
+                const sourceTierIndex = copiedTierList.findIndex((tier: { name: any; }) => tier.name === source.droppableId);
+                const destTierIndex = copiedTierList.findIndex((tier: { name: any; }) => tier.name === destination.droppableId);
+                const [removed] = copiedTierList[sourceTierIndex].items.splice(source.index, 1);
+                copiedTierList[destTierIndex].items.splice(destination.index, 0, removed);
+                tierlistDispatch({
+                    type: "UPDATE_TIERS",
+                    payload: {
+                        tiers: copiedTierList
+                    }
+                });
             }
         } else {
             if (source.droppableId === "0") {
                 const copiedTierList = [...tierList.tiers];
                 const [removed] = copiedTierList.splice(source.index, 1);
                 copiedTierList.splice(destination.index, 0, removed);
-                setTierList((prev:TierList) => ({
-                    ...prev,
-                    tiers: copiedTierList
-                }));
+                tierlistDispatch({
+                    type: "UPDATE_TIERS",
+                    payload: {
+                        tiers: copiedTierList
+                    }
+                });
             } else {
                 const tier = tierList.tiers.find((tier: { name: any; }) => tier.name === source.droppableId);
                 if (!tier) return;
                 const copiedItems = [...tier.items];
                 const [removed] = copiedItems.splice(source.index, 1);
                 copiedItems.splice(destination.index, 0, removed);
-                setTierList((prev:TierList) => ({
-                    ...prev,
-                    tiers: prev.tiers.map((tier: { name: any; items: any; }) => {
-                        if (tier.name === source.droppableId) {
-                            return {
-                                ...tier,
-                                items: copiedItems,
-                            }
-                        }
-                        return tier;
-                    }),
-                }));
+                tierlistDispatch({
+                    type: "DRAG_DROP_ITEM",
+                    payload: {
+                        tierIndex: tierList.tiers.indexOf(tier),
+                        newItems: copiedItems,
+                    }
+                });
             }
         }
     };
 
-    const addItem = (tierName: string, item: ItemProps) => {
-        setTierList((prev:TierList) => ({
-            ...prev,
-            tiers: prev.tiers.map((tier: { name: any; items: any; }) => {
-                if (tier.name === tierName) {
-                    return {
-                        ...tier,
-                        items: [...tier.items, item],
-                    };
-                }
-                return tier;
-            }),
-        }));
+    const handleAddTier = () => {
+        tierlistDispatch({type: "ADD_TIER", payload: {name: `T${tierList.tiers.length + 1}`, color: colors[tierList.tiers.length % colors.length], items: []}});
     };
 
-    const changeTierColor = (tierName: string, color: string) => {
-        setTierList((prev:TierList) => ({
-            ...prev,
-            tiers: prev.tiers.map((tier: { name: any; items: any; }) => {
-                if (tier.name === tierName) {
-                    return {
-                        ...tier,
-                        color: color,
-                    };
-                }
-                return tier;
-            }),
-        }));
-    }
-
-    const removeItem = (itemName:string) => {
-        setTierList((prev:TierList) => ({
-            ...prev,
-            tiers: prev.tiers.map((tier: { name: any; items: any; }) => {
-                return {
-                    ...tier,
-                    items: tier.items.filter((item: ItemProps) => item.id !== itemName),
-                }
-            }),
-        }));
-    };
-
-    const handleTierNameChange = (oldName: string, newName: string) => {
-        setTierList((prev:TierList) => ({
-            ...prev,
-            tiers: prev.tiers.map((tier: { name: any; items: any; }) => {
-                if (tier.name === oldName) {
-                    return {
-                        ...tier,
-                        name: newName,
-                    };
-                }
-                return tier;
-            }),
-        }));
-    };
-
-    const removeTier = (tierName: string) => {
-        setTierList((prev:TierList) => ({
-            ...prev,
-            tiers: prev.tiers.filter((tier: { name: any; }) => tier.name !== tierName),
-        }));
-    }
     useEffect(() => {
         setIsReady(true);
     }, []);
     return (
-        <DragDropContext onDragEnd={handleDragEnd}>
-            <input type="text" className={styles.tierListName} value={tierList.name} onChange={(e) => setTierList((prev:TierList) => ({
-                ...prev,
-                name: e.target.value
-            }))} disabled={!editable} placeholder={"Nom de la tierlist"} />
-            <Droppable droppableId="0" type="tier" isDropDisabled={!editable}>
-                {(provider) => (
-                    <div {...provider.droppableProps} ref={provider.innerRef} className={styles.tierList}>
+        <TierlistContext.Provider value={{tierList, tierlistDispatch, editable}}>
+            <DragDropContext onDragEnd={handleDragEnd}>
+                <input type="text" className={styles.tierListName} value={tierList.name} onChange={(e) => tierlistDispatch({type: "UPDATE_TIERLIST_NAME", payload: {name: e.target.value}})} disabled={!editable} placeholder={"Nom de la tierlist"} />
+                <Droppable droppableId="0" type="tier" isDropDisabled={!editable}>
+                    {(provider) => (
+                        <div {...provider.droppableProps} ref={provider.innerRef} className={styles.tierList}>
 
-                            {isReady && tierList.tiers.map((tier,index) => (
-                                <Tier key={tier.name} name={tier.name} color={tier.color} colors={colors} colorCallback={changeTierColor} items={tier.items} index={index} editable={editable} addItem={addItem} handleTierNameChange={handleTierNameChange} removeItem={removeItem} removeCallback={removeTier}/>
-                            ))}
-                        {provider.placeholder}
+                                {isReady && tierList.tiers.map((tier,index) => (
+                                    <Tier key={tier.name} name={tier.name} colors={colors} index={index} />
+                                ))}
+                            {provider.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+                {editable &&
+                    <div className={styles.addTier}>
+                        <button onClick={handleAddTier}>
+                            Ajouter un tier
+                        </button>
                     </div>
-                )}
-            </Droppable>
-            {editable &&
-                <div className={styles.addTier}>
-                    <button onClick={() => setTierList((prev:TierList) => ({
-                        ...prev,
-                        tiers: [...prev.tiers, {
-                            name: `T${prev.tiers.length + 1}`,
-                            color: colors[prev.tiers.length % colors.length],
-                            items: []
-                        }]
-                    }))}>
-                        Ajouter un tier
-                    </button>
-                </div>
-            }
-        </DragDropContext>
+                }
+            </DragDropContext>
+        </TierlistContext.Provider>
     );
 }
 
