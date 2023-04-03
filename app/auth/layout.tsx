3f3cb9f4885f3d0cc2c279508f750f74196edbb1
Daemon0x00000000 <kd.daemon.kd@gmail.com>
2023-04-03 22:29:21 +0200

    auth: add of Toaster

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,10 +1,12 @@
 "use client";
 import {SessionProvider} from "next-auth/react";
 import {ReactNode} from "react";
+import { Toaster } from "react-hot-toast";
 
 export default function AuthLayout({children}: { children: ReactNode }) {
     return (
         <SessionProvider>
+            <Toaster />
             {children}
         </SessionProvider>
     );
