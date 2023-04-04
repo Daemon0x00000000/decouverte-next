e721d76c4ea88964d8037cd9c487c720e15f8a50
Daemon0x00000000 <kd.daemon.kd@gmail.com>
2023-04-04 15:40:36 +0200

    fix: url

--- app/tierlists/page.tsx
+++ app/tierlists/page.tsx
@@ -3,8 +3,7 @@ import styles from "styles/Tierlists.module.scss";
 import TierListInterface from "../../interfaces/TierListInterface";
 const getData = async () => {
     // Endpoint is /api/tierlists, method is GET, revalidate is 10 seconds
-    const NEXT_PUBLIC_API_URL = process.env.NEXTAUTH_URL;
-    const res = await fetch(`${NEXT_PUBLIC_API_URL}/api/tierlists`, {
+    const res = await fetch(`https://decouverte-next.vercel.app/api/tierlists`, {
         next: {
             revalidate: 10
         }
