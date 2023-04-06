95e1d13b304fd3fe935c28b6f1d315fd0046bb5c
Daemon0x00000000 <kd.daemon.kd@gmail.com>
2023-04-06 21:50:28 +0200

    new: Refonte complet de l'interface, avec responsivité, menu hamburger, et dropdown pour le profil

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,46 +1,157 @@
+"use client";
+import {
+    Box,
+    Flex,
+    Avatar,
+    HStack,
+    IconButton,
+    Link,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+    useDisclosure,
+    useColorModeValue,
+    Stack,
+    ChakraProvider,
+    Skeleton, SkeletonCircle,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon, AddIcon, SettingsIcon } from '@chakra-ui/icons';
+import {ReactNode} from "react";
+import {useSession} from "next-auth/react";
+import NextLink from "next/link";
+import {FaStar} from "react-icons/all";
 import styles from "styles/Header.module.scss";
-import {authOptions} from "../pages/api/auth/[...nextauth]";
-import {getServerSession} from "next-auth";
-import {FaSignInAlt, FaSignOutAlt, FaPlusCircle} from "react-icons/fa";
-import Link from "next/link";
-import {FaStar} from "react-icons/fa";
+const NavLink = ({ href, children }: { href: string, children: ReactNode }) => (
+    <Link
+        as={NextLink}
+        px={3}
+        py={1}
+        fontSize={'md'}
+        rounded={'md'}
+        _hover={{
+            textDecoration: 'none',
+            bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={href}>
+        {children}
+    </Link>
+);
 
-export default async function Header() {
-    const session = await getServerSession(authOptions);
+export default function Header() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const {data: session, status} = useSession();
+
+    const links = [
+        {
+            name: 'Tierlists',
+            href: '/tierlists'
+        }
+    ]
     return (
-        <header className={styles.header}>
-            <nav className={styles.nav}>
-                <ul className={styles.list}>
-                    <li className={styles.item}><Link className={styles.title} href="/">TierList R<FaStar style={{color: "#FFD700",fontSize:"1.2rem"}}/>nker</Link></li>
-                    <div className={styles.clickable}>
-                        <li className={styles.demo}>
-                            <Link href="/tierlists/create">
-                                <FaPlusCircle />
-                                <span>{session ? 'Ajouter' : 'Demo'}</span>
-                            </Link>
-                        </li>
-                        <li className={styles.item}>
-                            <Link href="/tierlists">
-                                <span>Liste des tierlists</span>
-                            </Link>
-                        </li>
-                        <li className={styles.item}>
-                            {!session ? (
-                            <Link href="/auth/signin">
-                                <FaSignInAlt/>
-                                <span>Connexion/Inscription</span>
-                            </Link>
-                            ) : (
-                            <Link href="/auth/signout">
-                                <FaSignOutAlt/>
-                                <span>Déconnexion</span>
-                            </Link>
-                            )}
-                        </li>
-                    </div>
-                </ul>
-            </nav>
+        <ChakraProvider>
+            <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+                <Flex h={20} alignItems={'center'} justifyContent={'space-between'}>
+                    <IconButton
+                        size={'md'}
+                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                        aria-label={'Open Menu'}
+                        display={{ md: 'none' }}
+                        onClick={isOpen ? onClose : onOpen}
+                    />
+                    <HStack spacing={8} alignItems={'center'}>
+                        <HStack
+                            as={'nav'}
+                            spacing={4}
+                            display={{ base: 'none', md: 'flex' }}>
+                            <Box fontSize={'xl'} mr={4}>
+                                <Link as={NextLink} href={'/'} display={'flex'} alignItems={'center'} justifyContent={'center'} className={styles.title}>
+                                    Tierlist R<FaStar color={'#FFD700'} size={20}/>nker
+                                </Link>
+                            </Box>
+                            {links.map((link,index) => (
+                                <NavLink href={link.href} key={index}>{link.name}</NavLink>
+                            ))}
+                        </HStack>
+                    </HStack>
+                    <Flex alignItems={'center'}>
+                        {status === 'loading' ? (
+                            // Light radius
+                            <Skeleton w={24} h={10} rounded={'full'} mr={4} />
+                        ) : (
+                            <Button
+                                variant={'solid'}
+                                colorScheme={'teal'}
+                                size={'sm'}
+                                rounded={'full'}
+                                mr={4}
+                                leftIcon={<AddIcon />}
+                                w={24}
+                                h={10}
+                            >
+                                <NextLink href={'/tierlists/create'}>
+                                    {session ? 'Ajouter' : 'Demo'}
+                                </NextLink>
+                            </Button>
+                        )}
+                        <Menu>
+                            <MenuButton
+                                as={Button}
+                                rounded={'full'}
+                                variant={'link'}
+                                cursor={'pointer'}
+                                _hover={
+                                    {
+                                        textDecoration: 'none',
+                                        bg: useColorModeValue('gray.300', 'gray.700'),
+                                    }
+                                }
+                                minW={0}>
+                                {status === 'loading' ? (
+                                     <SkeletonCircle size="10" />
+                                ) : (
+                                <Avatar
+                                    size={'sm'}
+                                    src={session ? 'https://img.icons8.com/officel/256/user.png' : ''}
+                                />
+                                )}
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem><SettingsIcon mr={2}/>Paramètres</MenuItem>
+                                <MenuDivider />
+                                <HStack p={4} mt={2} justifyContent={'center'}>
+                                    {!session ? (
+                                        <>
+                                            <Button colorScheme="blue" variant="solid">
+                                                <Link href="/auth/signin">Connexion</Link>
+                                            </Button>
+                                            <Button colorScheme="blue" variant="outline">
+                                                <Link href="/auth/signup">Inscription</Link>
+                                            </Button>
+                                        </>
+                                    ) : (
+                                        <Button colorScheme="red" variant="outline">
+                                            <Link href="/auth/signout">Déconnexion</Link>
+                                        </Button>
+                                    )}
+                                </HStack>
+                            </MenuList>
+                        </Menu>
+                    </Flex>
+                </Flex>
 
-        </header>
+                {isOpen ? (
+                    <Box pb={4} display={{ md: 'none' }}>
+                        <Stack as={'nav'} spacing={4}>
+                            {links.map((link,index) => (
+                                <NavLink href={link.href} key={index}>{link.name}</NavLink>
+                            ))}
+                        </Stack>
+                    </Box>
+                ) : null}
+            </Box>
+        </ChakraProvider>
     );
 }
