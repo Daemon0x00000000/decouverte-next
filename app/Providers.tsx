3e81ca291d9e8a5d413d0d3f23fc1194bb61631c
Daemon0x00000000 <kd.daemon.kd@gmail.com>
2023-04-09 19:10:31 +0200

    Providers: ajout des dev tools + chakra et Hot toast

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -2,6 +2,9 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { SessionProvider } from 'next-auth/react';
 import {ReactNode, useState} from "react";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {Toaster} from "react-hot-toast";
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default function Providers({ children }: { children: ReactNode }) {
     const [queryClient] = useState(() => new QueryClient())
@@ -9,8 +12,12 @@ export default function Providers({ children }: { children: ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
             <SessionProvider>
-                {children}
+                <ChakraProvider>
+                    <Toaster />
+                    {children}
+                </ChakraProvider>
             </SessionProvider>
+            <ReactQueryDevtools initialIsOpen={true} />
         </QueryClientProvider>
     )
 }
