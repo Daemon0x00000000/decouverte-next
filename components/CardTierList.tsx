069a0e652f52d9883411e23d91a1869d42f52a97
Daemon0x00000000 <kd.daemon.kd@gmail.com>
2023-04-09 19:19:41 +0200

    ui: Nouvelle card

--- components/CardTierList.tsx
+++ components/CardTierList.tsx
@@ -1,8 +1,8 @@
-import styles from "styles/CardTierList.module.scss";
-import {TextBlock, RectShape} from "react-placeholder/lib/placeholders";
+"use client";
 import  'react-placeholder/lib/reactPlaceholder.css';
-import Image from "next/image";
 import Link from "next/link";
+import {Box, Button, ButtonGroup, Flex, IconButton, Image, Skeleton} from "@chakra-ui/react";
+import {ArrowForwardIcon, ArrowUpIcon, ArrowDownIcon} from "@chakra-ui/icons";
 
 type CardData = {
     id:string,
@@ -12,22 +12,61 @@ type CardData = {
 export default function CardTierList({loading=true, data}: {loading?: boolean, data?: CardData}) {
     const {name, media} = data || {name: "", media: ""};
     return (
-        <div className={styles.card}>
+        <Flex p={50} alignItems="center" justifyContent="center">
             {loading ? (
-                <RectShape color={"#f0f0f0"} style={{width: "100%", height: "100%"}} />
+                <Skeleton width={500} height={350} rounded={"lg"} />
             ) : (
-                <Image src={media} alt={name} layout={"fill"} objectFit={"cover"} className={styles.card__img} />
+            <Box
+                bg={'white'}
+                maxW="lg"
+                borderWidth="1px"
+                rounded="lg"
+                shadow="lg"
+                position="relative">
+
+                <Image
+                    src={media}
+                    alt={`Picture of`}
+                    roundedTop="lg"
+                    minH={200}
+                    maxH={200}
+                    minW={500}
+                    maxW={500}
+                />
+
+                <Box p="6">
+                    <Flex mt="1" justifyContent="space-between" alignContent="center">
+                        <Box
+                            fontSize="2xl"
+                            fontWeight="semibold"
+                            as="h4"
+                            lineHeight="tight"
+                            isTruncated>
+                            {name}
+                        </Box>
+                    </Flex>
+
+                    <Flex justifyContent="space-between" alignContent="center">
+                        <Box fontSize="2xl" color={'gray.800'}>
+                            20000
+                            <ButtonGroup size="md" isAttached m={2} ml={4} gap={1}>
+                                <IconButton aria-label={"Upvote"} icon={<ArrowUpIcon/>} rounded={"full"}
+                                            colorScheme={"green"} variant={"outline"}/>
+                                <IconButton aria-label={"Downvote"} icon={<ArrowDownIcon/>} rounded={"full"}
+                                            colorScheme={"red"} variant={"outline"}/>
+                            </ButtonGroup>
+                        </Box>
+                        <Box fontSize="2xl" color={'gray.800'}>
+                                <Button rightIcon={<ArrowForwardIcon/>} colorScheme='teal' variant='outline'>
+                                    <Link href={`/tierlists/${data?.id}`}>
+                                        Voir
+                                    </Link>
+                                </Button>
+                        </Box>
+                    </Flex>
+                </Box>
+            </Box>
             )}
-            <div className={styles.card__footer}>
-                {loading ? (
-                    <span><TextBlock color={"#f0f0f0"} rows={1} style={{width: "100%", height: "100%"}} /></span>
-                ) : (
-                    <>
-                        <span>{name}</span>
-                        <Link href={`/tierlists/${data?.id}`}>Voir</Link>
-                    </>
-                )}
-            </div>
-        </div>
+        </Flex>
     )
 }
