472aeaaae6d83ce5dcd26004f818f9264bf36cc9
Daemon0x00000000 <kd.daemon.kd@gmail.com>
2023-04-09 19:18:57 +0200

    edit: Utilisation de l'hydrated tierlists

--- app/tierlists/page.tsx
+++ app/tierlists/page.tsx
@@ -1,42 +1,12 @@
-"use client";
-import CardTierList from "../../components/CardTierList";
 import styles from "styles/Tierlists.module.scss";
-import TierListInterface from "../../interfaces/TierListInterface";
-import {useEffect} from "react";
-import { useState } from "react";
-const getData = async () => {
-    // Endpoint is /api/tierlists, method is GET, revalidate is 10 seconds
-    const res = await fetch(`/api/tierlists`, {
-        method: "GET",
-    });
-    if (!res.ok) {
-        throw new Error(res.statusText);
-    }
+import HydratedTierlists from "./HydratedTierlists";
 
-    return await res.json();
-}
-
-export default function TierlistsPage() {
-    const [data, setData] = useState<TierListInterface[]>([]);
-
-    useEffect(() => {
-        getData().then((data) => {
-            setData(data);
-        });
-    }, []);
+export default async function TierlistsPage() {
+    const hydrate = await HydratedTierlists()
     return (
         <>
             <h1 className={styles.title}>Liste des tierlists</h1>
-            <div className={styles.grid}>
-                {data && data.map((tierlist:TierListInterface, i:number) => (
-                    <CardTierList key={i} data={{
-                        id: tierlist.id as string,
-                        name: tierlist.name,
-                        media: tierlist.media,
-                    }
-                    } loading={false} />
-                ))}
-            </div>
+            {hydrate}
         </>
     )
 }
