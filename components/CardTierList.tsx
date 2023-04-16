5d2635a3d73d065431f702de92aad8738f8e7b73
Daemon0x00000000 <kd.daemon.kd@gmail.com>
2023-04-16 22:03:43 +0200

    CardTierlist : Implémentation du système de votes

--- components/CardTierList.tsx
+++ components/CardTierList.tsx
@@ -1,16 +1,63 @@
 "use client";
-import  'react-placeholder/lib/reactPlaceholder.css';
 import Link from "next/link";
-import {Box, Button, ButtonGroup, Flex, IconButton, Image, Skeleton} from "@chakra-ui/react";
+import {Box, Button, ButtonGroup, Flex, IconButton, Image, Skeleton, useBoolean} from "@chakra-ui/react";
 import {ArrowForwardIcon, ArrowUpIcon, ArrowDownIcon} from "@chakra-ui/icons";
+import {useCallback, useEffect} from "react";
+import {useMutation} from "@tanstack/react-query";
+import {toast} from "react-hot-toast";
 
 type CardData = {
     id:string,
     name: string,
     media: string,
+    votes: {points:string}[],
+    score: number
 }
+
+
 export default function CardTierList({loading=true, data}: {loading?: boolean, data?: CardData}) {
     const {name, media} = data || {name: "", media: ""};
+    const [upvote, setUpvote] = useBoolean();
+    const [downvote, setDownvote] = useBoolean();
+    const isVoted = useCallback((voteType:string) => {
+        return data?.votes.find(vote => vote.points == (voteType === 'upvote' ? '1' : '-1')) !== undefined;
+    }, [data?.votes]);
+
+    const toggleVote = useCallback((voteType:string) => {
+        voteType === 'upvote'
+            ? (downvote ? setDownvote.toggle() : null, setUpvote.toggle())
+            : (upvote ? setUpvote.toggle() : null, setDownvote.toggle());
+    }, [downvote, upvote, setDownvote, setUpvote]);
+
+    const vote = useMutation(async (voteType:string) => {
+        const res = await fetch(`/api/tierlists/vote`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: data?.id,
+                strVote: voteType === 'upvote' ? isVoted(voteType) ? '0' : '1' : isVoted(voteType) ? '0' : '-1'
+            })
+        })
+        return res.json();
+    }, {
+        onError: () => {
+            toast.error("Une erreur est survenue lors du vote");
+        }
+    })
+
+    const handleVote = useCallback((voteType:string) => {
+        toggleVote(voteType);
+        vote.mutate(voteType);
+    }, [toggleVote, vote]);
+
+    useEffect(() => {
+        console.log(data)
+        if (isVoted('upvote')) setUpvote.on();
+        if (isVoted('downvote')) setDownvote.on();
+    }, [data, data.votes, isVoted, setDownvote, setUpvote]);
+
     return (
         <Flex p={50} alignItems="center" justifyContent="center">
             {loading ? (
@@ -48,12 +95,12 @@ export default function CardTierList({loading=true, data}: {loading?: boolean, d
 
                     <Flex justifyContent="space-between" alignContent="center">
                         <Box fontSize="2xl" color={'gray.800'}>
-                            20000
+                            {data?.score}
                             <ButtonGroup size="md" isAttached m={2} ml={4} gap={1}>
                                 <IconButton aria-label={"Upvote"} icon={<ArrowUpIcon/>} rounded={"full"}
-                                            colorScheme={"green"} variant={"outline"}/>
+                                            colorScheme={"green"} onClick={() => handleVote('upvote')} variant={upvote ? "solid" : "outline"}/>
                                 <IconButton aria-label={"Downvote"} icon={<ArrowDownIcon/>} rounded={"full"}
-                                            colorScheme={"red"} variant={"outline"}/>
+                                            colorScheme={"red"} onClick={() => handleVote('downvote')} variant={downvote ? "solid" : "outline"}/>
                             </ButtonGroup>
                         </Box>
                         <Box fontSize="2xl" color={'gray.800'}>
