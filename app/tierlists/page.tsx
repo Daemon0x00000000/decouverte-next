d555c70e923c48c5075a0024934f3e87747d2ad1
Daemon0x00000000 <kd.daemon.kd@gmail.com>
2023-04-16 22:05:00 +0200

    edit: Plus de pre-fetch, SSR => CSR

--- app/tierlists/page.tsx
+++ app/tierlists/page.tsx
@@ -1,12 +1,11 @@
 import styles from "styles/Tierlists.module.scss";
-import HydratedTierlists from "./HydratedTierlists";
+import Tierlists from "../../components/Tierlists";
 
 export default async function TierlistsPage() {
-    const hydrate = await HydratedTierlists()
     return (
         <>
             <h1 className={styles.title}>Liste des tierlists</h1>
-            {hydrate}
+            <Tierlists/>
         </>
     )
 }
