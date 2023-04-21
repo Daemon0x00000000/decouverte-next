a6538972cf6f67634ace753a2c7b004ee2cca180
Daemon0x00000000 <kd.daemon.kd@gmail.com>
2023-04-21 21:39:46 +0200

    signup: adaptation pour le composant fix: redirect

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 import {signIn, useSession} from "next-auth/react";
-import {FormEventHandler, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import {redirect} from "next/navigation";
-import styles from "styles/Signup.module.scss";
-import Link from "next/link";
 import {toast} from "react-hot-toast";
 import { useMutation } from "@tanstack/react-query";
+import {SignupForm} from "../../../components/SignupForm";
 
-type User = {
+export type User = {
     email: string,
     username: string,
     password: string,
@@ -43,15 +42,15 @@ export default function SignUpPage() {
             setLoading(true);
         },
         onSuccess: () => {
-            toast.success("Vous êtes inscris !");
+
             signIn("credentials", {
                 email: user.email,
                 password: user.password,
-                redirect: true,
+                redirect: false,
                 callbackUrl: "/",
             }).then((res) => {
                 if (res && res.error) {
-                    toast.error("Une erreur est survenue lors de la connexion");
+                    toast.error("Une erreur est survenue, assurez vous que le username et l'email sont uniques");
                 }
             });
         },
@@ -63,35 +62,10 @@ export default function SignUpPage() {
         }
     });
 
-    const handleSubmit: FormEventHandler<HTMLInputElement> = (e) => {
-        e.preventDefault();
-        mutation.mutate({
-            email: user.email,
-            username: user.username,
-            password: user.password,
-        });
+    const handleSubmit = () => {
+        mutation.mutate(user);
     }
     return (
-        <div className={styles.container}>
-            <form className={styles.form}>
-                {/* eslint-disable-next-line react/no-unescaped-entities */}
-                <h1 className={styles.title}>S'inscrire</h1>
-                <div className={styles.inputContainer}>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} />
-                </div>
-                <div className={styles.inputContainer}>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} />
-                </div>
-                <div className={styles.inputContainer}>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} />
-                </div>
-                <input type="submit" className={styles.submit} onClick={handleSubmit} value={"Sign Up"} style={{cursor: loading ? "not-allowed" : "pointer"}} disabled={loading} />
-                {/* eslint-disable-next-line react/no-unescaped-entities */}
-                <Link href="/auth/signin">J'ai déjà un compte</Link>
-            </form>
-        </div>
+            <SignupForm onSubmit={handleSubmit} loading={loading} user={user} setUser={setUser} />
     );
 }
